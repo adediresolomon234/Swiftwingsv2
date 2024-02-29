c7aee3218b3a5bc2d9fb96c6dc8bfb1e580ab225
codeAmbivert <titilopechisom20@gmail.com>
2024-02-29 11:57:50 +0100

    sign-in and reusable input field

---
+++ app/components/shared/InputField.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const InputField = ({
+  css,
+  label,
+  placeholder,
+  inputType,
+  borderColor,
+  onChange,
+  value,
+  name,
+  endIcon,
+  startIcon,
+  disabled,
+}) => {
+  return (
+    <div className="">
+      {label && (
+        <label
+          htmlFor={name}
+          className="font-medium text-swGray900 mb-2 text-lg"
+        >
+          {label}
+        </label>
+      )}
+
+      <div
+        className={`${css} relative flex items-center cursor-pointer text-swGray900 hover:border-swPrimary500 rounded-lg overflow-hidden border ${borderColor} focus:outline-none cursor-pointer ${
+          startIcon ? "pl-8" : ""
+        } ${endIcon ? "pr-8" : ""}`}
+      >
+        {startIcon && (
+          <div className="absolute inset-y-0 left-3 flex items-center">
+            {startIcon}
+          </div>
+        )}
+
+        <input
+          type={inputType ? inputType : "text"}
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          className={`w-full h-11 px-3 py-2 cursor-pointer focus:outline-none`}
+          onChange={onChange}
+          value={value}
+          disabled={disabled}
+        />
+
+        {endIcon && (
+          <div className="absolute inset-y-0 right-0 pr-3 pt-5 pb-5 flex items-center">
+            {endIcon}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InputField;
