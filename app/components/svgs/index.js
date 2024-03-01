4c58175d90abef186b4809fc42aab3f18067a67d
adediresolomon <Solomonoluwaseun933@gmail.com>
2024-03-01 12:17:57 +0000

    update

--- app/components/svgs/index.js
+++ app/components/svgs/index.js
@@ -16,3 +16,5 @@ export { default as SwMailIcon } from "./mailIcon.svg";
 export { default as SwOpenEyeIcon } from "./openEyeIcon.svg";
 export { default as SwGoogleColoredIcon } from "./googleColoredIcon.svg";
 export { default as SwPlusIcon } from "./plusIcon.svg";
+export { default as SWArrowsChevron } from "./Arrows-chevron.svg";
+
