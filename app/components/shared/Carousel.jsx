ea14d5436ff46f3cb28985143e406aa3284ddce1
Babajide Ojo <babajide.ojo@konga.com>
2024-03-26 14:06:28 +0100

    chore

--- app/components/shared/Carousel.jsx
+++ app/components/shared/Carousel.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Carousel from "react-spring-3d-carousel";
+//import Carousel from "react-spring-3d-carousel";
 import { useState, useEffect } from "react";
 //import { config } from "react-spring";
 
