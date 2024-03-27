f000915caa1ce68f9de26ebe2dfbf2ac0a711067
adediresolomon <Solomonoluwaseun933@gmail.com>
2024-03-27 14:18:39 +0000

    update

---
+++ app/components/shared/SortFilter.jsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from 'react';
+import { SWArrowsChevron,SWFilterIcon} from "../svgs";
+
+const CustomFilter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = ['Airplane', 'Airplane', 'Airplane'];
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative w-full">
+     <label htmlFor="title" className="block font-medium text-swGray900 mb-2 text-lg flex items-center">
+        <SWFilterIcon className="mr-2 h-6 w-6" />
+        Filter
+      </label>
+      <button onClick={() => setIsOpen(!isOpen)} className="relative border border-gray-300 rounded-md px-4 py-2 w-full text-left">
+        {selectedOption || 'Sort by'}
+        <SWArrowsChevron className="absolute right-3 top-1/2 transform -translate-y-1/2 text-xl" />
+      </button>
+      {isOpen && (
+        <div className="absolute w-full mt-2 border border-gray-300 rounded-md shadow-md bg-white z-10">
+          {options.map((option) => (
+            <div key={option} onClick={() => handleSelect(option)} className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+  
+  export default CustomFilter ;
+  
--- app/fleet-page/page.js
+++ app/fleet-page/page.js
@@ -4,7 +4,11 @@ import Image from "next/image";
 import Fleetsection from "../../public/images/Fleetsection.png";
 import NavAndFooter from "../components/shared/NavAndFooter";
 import FooterHero from "../components/shared/footerHero";
-
+import InputField from "../components/shared/InputField";
+import { SwSearchIcon, SWFilterIcon, } from "../components/svgs";
+import SortFilter from "../components/shared/SortFilter";
+import TypeFilter from "../components/shared/TypeFilter";
+import { mdiCarSeat, mdiSpeedometer, mdiArrowLeftRight } from '@mdi/js'
 
 const spaceGrotesk = Space_Grotesk({
     subsets: ["latin"],
@@ -35,16 +39,110 @@ const FleetPage = () => {
                 </section>
                 <section className="py-16">
                     <div className="grid grid-cols-3 gap-4">
-                        <div className=""></div>
-                        <div className="col-span-2 "></div>
+                        <div className="p-12 max-w-sm w-full  ">
+                            <div className="w-full mt-5">
+                                <InputField
+                                    label={<span className="flex items-center mb-3 "><SwSearchIcon className="text-xl mr-2" />Search</span>}
+                                    placeholder="Enter Aircraft "
+                                />
+                            </div>
+                            <div className="w-full mt-12">
+                                <SortFilter />
+                            </div>
+                            <div className="w-full mt-3">
+                                <TypeFilter />
+                            </div>
+                        </div>
+
+                        <div className="col-span-2 ">
+                            <div className="max-w-full grid gap-8 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 grid-rows-2 gap-y-8 justify-center items-center relative">
+                                <div className="w-84 flex flex-col items-start justify-start pt-2 px-2 pb-[width] box-border gap-2 text-center text-base text-black font-body-xs-regular">
+                                    <Image
+                                        className="w-80 rounded-md h-43 object-cover"
+                                        alt="Aircraft Image"
+                                        src="/images/Features1.png"
+                                        width={80}
+                                        height={43}
+                                    />
+                                    <div className="self-stretch relative leading-6 font-medium">
+                                    Bombardier Global 8000
+                                    </div>
+                                    <div className="self-stretch flex flex-row items-center justify-between py-0 px-[width] text-left text-xs text-gray-800">
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                        <mdiCarSeat/>
+                                            <div className="relative leading-4.5">3 seats</div>
+                                        </div>
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                       < mdiSpeedometer/>
+                                            <div className="relative leading-4.5"> 5km/h</div>
+                                        </div>
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                            <mdiArrowLeftRight/>
+                                            <div className="relative leading-4.5">6feet</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="w-84 flex flex-col items-start justify-start pt-2 px-2 pb-[width] box-border gap-2 text-center text-base text-black font-body-xs-regular">
+                                    <Image
+                                        className="w-80 rounded-md h-43 object-cover"
+                                        alt="Aircraft Image"
+                                        src="/images/Features1.png"
+                                        width={80}
+                                        height={43}
+                                    />
+                                    <div className="self-stretch relative leading-6 font-medium">
+                                    Bombardier Global 8000
+                                    </div>
+                                    <div className="self-stretch flex flex-row items-center justify-between py-0 px-[width] text-left text-xs text-gray-800">
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                        <mdiCarSeat/>
+                                            <div className="relative leading-4.5">3 seats</div>
+                                        </div>
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                       < mdiSpeedometer/>
+                                            <div className="relative leading-4.5"> 5km/h</div>
+                                        </div>
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                            <mdiArrowLeftRight/>
+                                            <div className="relative leading-4.5">6feet</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="w-84 flex flex-col items-start justify-start pt-2 px-2 pb-[width] box-border gap-2 text-center text-base text-black font-body-xs-regular">
+                                    <Image
+                                        className="w-80 rounded-md h-43 object-cover"
+                                        alt="Aircraft Image"
+                                        src="/images/Features1.png"
+                                        width={80}
+                                        height={43}
+                                    />
+                                    <div className="self-stretch relative leading-6 font-medium">
+                                    Bombardier Global 8000
+                                    </div>
+                                    <div className="self-stretch flex flex-row items-center justify-between py-0 px-[width] text-left text-xs text-gray-800">
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                        <mdiCarSeat/>
+                                            <div className="relative leading-4.5">3 seats</div>
+                                        </div>
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                       < mdiSpeedometer/>
+                                            <div className="relative leading-4.5"> 5km/h</div>
+                                        </div>
+                                        <div className="flex flex-row items-center justify-start gap-2.5">
+                                            <mdiArrowLeftRight/>
+                                            <div className="relative leading-4.5">6feet</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </section>
                 <section className="py-16">
-                    <div class="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-16">
+                    <div className="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-16">
                         <FooterHero />
                     </div>
                 </section>
-
             </NavAndFooter>
         </main>
     );
