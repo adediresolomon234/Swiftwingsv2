56e1ce48869faff11e9b6a1259f3fbb487b53ea8
codeAmbivert <titilopechisom20@gmail.com>
2024-04-30 18:02:09 +0100

    booking responsiveness

--- app/page.js
+++ app/page.js
@@ -85,7 +85,7 @@ export default function Home() {
   return (
     <main className="relative bg-swLightBgGray">
      <NavAndFooter Nav={true}>
-        <section className="w-full p-10 pt-48 text-white relative pb-10">
+        <section className="w-full p-5 md:10 pt-48 text-white relative pb-10">
           <div className="absolute h-full w-full top-0 left-0">
             {isMobile ? (
               <Image src={MbheroBgImg} alt="aiplane" className="h-full w-full object-cover" />
