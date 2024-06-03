57d0d57bdc957111bb8aa51e6232033b2efd9e75
adediresolomon <Solomonoluwaseun933@gmail.com>
2024-06-03 14:55:43 +0000

    Update footerHero.jsx

--- app/components/shared/footerHero.jsx
+++ app/components/shared/footerHero.jsx
@@ -2,34 +2,39 @@ import Button from "../Button";
 import React from 'react';
 
 const FooterHero = () => {
-    return (
-            <div className="xl:container m-auto py-20 px-8 md:px-16 lg:px-16 overflow-hidden bg-floralwhite">
-                <div className="w-full relative flex flex-row items-start justify-between pb-[276px] pr-[236px] pl-[201px] box-border tracking-[normal] gap-[20px] text-left text-xl text-gray-900 font-header-sm-semi-bold lg:flex-wrap lg:pl-5 lg:pr-5 lg:box-border lg:pl-[195px] lg:pr-[120px] md:box-border">
-                <section className="w-full absolute  bottom-[-140px] left-[calc(50%_-_676px)] rounded-t-45xl rounded-b-none bg-floralwhite" />
-                <img
-                  className="h-[826px] w-[706px] absolute  right-[-200px] bottom-[-312px] object-cover z-[1]"
-                  loading="lazy"
-                  alt=""
-                  src="/images/Circles.png"
-                />
-                <div className="w-[440px] relative leading-[32px] font-semibold inline-block shrink-0 max-w-full z-[2] ">
-                  You’re one click away from booking the perfect Jet for your journey.
-                </div>
-                <div className="absolute right-[200px] bottom-[180px] z-[2]">
-                  <Button
-                    label="Book a jet"
-                    bgColor="bg-swPrimary500"
-                    textColor="text-white"
-                    className="h-12 px-6 text-base"
-                  />
-                </div>
-                <img
-                  className="h-[500px] w-[650px] absolute  bottom-[-80.4px ] left-0 lg:left-[-9.5px] object-contain z-[1]"
-                  alt=""
-                  src="/images/airplane-footer.png"
-                />
-              </div>
+  return (
+    <div className="bg-floralwhite py-20 px-8 md:px-16 lg:px-16 overflow-hidden">
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-8">
+        <div className="flex-1 w-[440px] relative leading-[32px] font-semibold inline-block shrink-0 max-w-full z-[2]">
+          <div className="p-8 max-w-[400px]">
+           You're one click away from booking the perfect Jet for your journey.
+          </div>
+          <img
+            className="absolute lg:w-full lg:max-w-[850px] mx-auto lg:mx-0 mt-0 lg:right-36  lg:mt-[-145px]"
+            alt=""
+            src="/images/airplane-footer.png"
+          />
+        </div>
+        <div className="flex-1 flex justify-center">
+          <div className="h-[826px] w-[706px] relative  lg:right-[-200px] lg: bottom-[-312px] object-cover z-[1]">
+            <img
+              className="w-full "
+              loading="lazy"
+              alt=""
+              src="/images/Circles.png"
+            />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <Button
+                label="Book a jet"
+                bgColor="bg-swPrimary500"
+                textColor="text-white"
+                className="h-12 px-6 text-base"
+              />
             </div>
-            );
-        };
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default FooterHero;
