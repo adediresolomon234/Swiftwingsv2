b962005c710bb67c12177539d5e481ec4cb7f8b8
--replace-all <solomonoluwaseun933@gmail.com>
2024-09-01 19:14:38 +0100

    Update page.js

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -255,58 +255,6 @@ const ContactUs = () => {
             </div>
           </div>
         </section>
-        <section className="py-16 mb-0 md:mb-8 lg:mb-16">
-          <div className="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-16">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-40 lg:gap-16">
-              <div className="mb-30">
-                <div className="flex relative">
-                  <div className="w-72 h-60 bg-swPrimary700 card-container-3 transform transition-all absolute rounded-lg top-3 -left-6"></div>
-                  <div className="w-72 h-72 card-container-2 bg-swPrimary600 transform transition-all absolute -top-2 -left-12 rounded-lg"></div>
-                  <div className="w-full card-container md:w-80 h-full md:h-80 bg-swPrimary500 flex flex-col justify-start items-center transform transition-all absolute -top-6 md:-left-20 rounded-lg">
-                    <h2 className="text-2xl font-bold md:text-4xl text-white px-12 py-4">
-                      Frequently Asked Questions
-                    </h2>
-                    <p className="text-md px-12 mb-8 lg:mb-0 md:text-xl text-white">
-                      Here are previously answered questions.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-12 px-4 lg:mt-0">
-                <div>
-                  {accordions.map((accordion, index) => (
-                    <Accordion
-                      key={accordion.id}
-                      expanded={activeAccordion === index}
-                      onChange={() => toggleAccordion(index)}
-                    >
-                      <AccordionSummary
-                        aria-controls={`panel${accordion.id}-content`}
-                        id={`panel${accordion.id}-header`}
-                        className="font-bold"
-                        expandIcon={
-                          activeAccordion === index ? (
-                            <SWTAccordionsOpenIcon />
-                          ) : (
-                            <SWTAccordionsCloseIcon />
-                          )
-                        }
-                      >
-                        {accordion.header}
-                      </AccordionSummary>
-                      <AccordionDetails>{accordion.content}</AccordionDetails>
-                      {accordion.id === 3 && (
-                        <AccordionActions>
-                          {/* Add any actions here if needed */}
-                        </AccordionActions>
-                      )}
-                    </Accordion>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
         <section className=" lg:py-60 mb-0 lg:-mb-60">
           <div className="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-16">
             <FooterHero />
