8776583de028c73515eee9167344bc085240031f
babajide.ojo <ojobabajide629@gmail.com>
2025-03-03 18:42:03 +0100

    chore

--- app/components/shared/EmptyLegs.jsx
+++ app/components/shared/EmptyLegs.jsx
@@ -6,9 +6,11 @@ import { IoAirplaneOutline } from "react-icons/io5";
 
 const EmptyLegsSlider = () => {
   const dispatch = useDispatch();
-  const { data: availableLegs, isLoading, error } = useSelector(
-    (state) => state.emptylegs
-  );
+  const {
+    data: availableLegs,
+    isLoading,
+    error,
+  } = useSelector((state) => state.emptylegs);
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const itemsPerPage = 4;
@@ -76,16 +78,24 @@ const EmptyLegsSlider = () => {
         Available Empty Legs
       </h2>
 
-      <div className="grid gap-4 md:mx-auto sm:grid-cols-2 lg:w-full xl:grid-cols-4">
+      <div className="grid gap-4 md:mx-auto sm:grid-cols-2 lg:w-full xl:grid-cols-4 mx-auto my-auto">
         {visibleLegs.map((leg, index) => (
           <div
             key={index}
-            className="bg-white border border-gray-200 group space-y-6 rounded-3xl  px-8 py-8 text-centertransition-all duration-300"
+            className="max-w-sm bg-white border border-gray-200 group space-y-6 rounded-3xl text-center transition-all duration-300"
           >
             <h5 className="mb-4 text-2xl font-semibold text-gray-800">
               {leg?.aircraft_name}
             </h5>
-            <div className="space-y-8">
+            <div className="space-y-8 bg-white shadow-lg rounded-lg p-6">
+              {/* SVG Image */}
+              <div className="w-full h-40 flex justify-center items-center bg-gray-100 rounded-lg">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLsQGGfvuHNbBk-49d3RDBcjP4uyZmCjiMgA&s"
+                  alt="empty-leg"
+                />
+              </div>
+
               <div className="flex items-center space-x-6 h-20">
                 <div className="flex items-center space-x-2">
                   <IoAirplaneOutline className="w-5 h-5 text-gray-900" />
@@ -101,28 +111,38 @@ const EmptyLegsSlider = () => {
                   </p>
                 </div>
               </div>
-              <p className="text-gray-700 text-base">
+
+              <p className="text-gray-700 text-sm">
                 <span className="font-semibold text-gray-900">Date:</span>{" "}
-                {new Date(leg?.dates).toLocaleDateString()}
+                {new Date(leg?.dates).toLocaleDateString("en-US", {
+                  weekday: "long",
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
               </p>
+
               <p className="text-gray-700 text-lg font-medium text-end">
                 ${new Intl.NumberFormat().format(leg?.price)}
               </p>
-            </div>
-            <div className="flex justify-center mt-6">
-              <Button
-                label="Book Now"
-                bgColor="bg-swPrimary500 hover:bg-swPrimary600"
-                textColor="text-white"
-                className="w-full px-6 py-3 rounded-lg shadow-md transition-all duration-300 text-lg font-semibold"
-              />
+
+              <div className="flex justify-center mt-6">
+                <Button
+                  label="Book Now"
+                  bgColor="bg-swPrimary500 hover:bg-swPrimary600"
+                  textColor="text-white"
+                  className="w-full px-6 py-3 rounded-lg shadow-md transition-all duration-300 text-lg font-semibold"
+                />
+              </div>
             </div>
           </div>
         ))}
       </div>
 
       <div className="flex justify-center mt-4 space-x-2">
-        {Array.from({ length: Math.ceil(availableLegs.length / itemsPerPage) }).map((_, index) => (
+        {Array.from({
+          length: Math.ceil(availableLegs.length / itemsPerPage),
+        }).map((_, index) => (
           <button
             key={index}
             onClick={() => setCurrentIndex(index)}
