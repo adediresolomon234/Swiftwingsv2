68adb6ec3fd154be90f1aed5bdca8fe212e3b05b
babajide.ojo <ojobabajide629@gmail.com>
2025-04-06 19:32:55 +0100

    chore

--- constant.js
+++ constant.js
@@ -1,2 +1,2 @@
-export const API_URL = `https://swiftwings-mw.onrender.com/api/v1`;
-export const BASE_URL = `https://swiftwings-mw.onrender.com`;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
