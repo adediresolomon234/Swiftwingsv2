46ef5c2694c3df71f23598c70c01141c6b779e8f
Titilope Chisom <titilopechisom20@gmail.com>
2025-09-16 17:43:48 +0100

    chore

--- app/pricing/page.jsx
+++ app/pricing/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useState } from "react";
 import { Check, X } from "lucide-react";
-import NavAndFooter from "../../components/shared/NavAndFooter";
-import SubImage from "../../../public/images/subImage.png";
+import SubImage from "../../public/images/subImage.png";
 import Head from "next/head";
 import Link from "next/link";
+import NavAndFooter from "../components/shared/NavAndFooter";
 
 export default function PricingPage() {
   const [activeTab, setActiveTab] = useState("yearly");
